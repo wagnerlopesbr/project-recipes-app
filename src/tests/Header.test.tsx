5afb5d33327cfb3f5262f22604c220b8740b6cb0
MateusDDev <ms4811978@gmail.com>
2023-10-25 08:06:43 -0300

    fix: tests

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -3,6 +3,31 @@ import { renderWithRouter } from './helpers/renderWithRouter';
 import App from '../App';
 
 describe('Verificando se o Header funciona corretamente', () => {
+  test('Verifica se Meals tem o título correto', () => {
+    renderWithRouter(<App />, { route: '/meals' });
+    expect(screen.getByText('Meals'));
+  });
+
+  test('Verifica se Drinks tem o título correto', () => {
+    renderWithRouter(<App />, { route: '/drinks' });
+    expect(screen.getByText('Drinks'));
+  });
+
+  test('Verifica se Profile tem o título correto', () => {
+    renderWithRouter(<App />, { route: '/profile' });
+    expect(screen.getByText('Profile'));
+  });
+
+  test('Verifica se Done Recipes tem o título correto', () => {
+    renderWithRouter(<App />, { route: '/done-recipes' });
+    expect(screen.getByText('Done Recipes'));
+  });
+
+  test('Verifica se Favorite Recipes tem o título correto', () => {
+    renderWithRouter(<App />, { route: '/favorite-recipes' });
+    expect(screen.getByText('Favorite Recipes'));
+  });
+
   test('Ao clica no botão de perfil é redirecionado para a rota "/profile"', async () => {
     const { user } = renderWithRouter(<App />, { route: '/meals' });
 
