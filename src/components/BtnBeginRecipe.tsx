9baa4c6e27b27122b8dfdef40972bc41baa56e65
Pedro-Teixeira <phstsam@gmail.com>
2023-10-26 15:30:32 -0300

    Botão start recipe

--- src/components/BtnBeginRecipe.tsx
+++ src/components/BtnBeginRecipe.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { DoneRecipesLocal } from '../Type/type';
 
@@ -11,24 +11,24 @@ function BtnBeginRecipe({ page, recipeId }: PropType) {
   const [recipesList, setRecipeList] = useState([] as DoneRecipesLocal[]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const storageDoneRecipes: DoneRecipesLocal[] = JSON
-      .parse(localStorage.getItem('doneRecipes') || 'null') ?? '[]';
-    setRecipeList(storageDoneRecipes);
-  }, []);
+  // useEffect(() => {
+  //   const storageDoneRecipes: DoneRecipesLocal[] = JSON
+  //     .parse(localStorage.getItem('doneRecipes') || 'null') ?? '[]';
+  //   setRecipeList(storageDoneRecipes);
+  // }, []);
 
   const isRecipeInProgress = () => {
     if (!recipeId) {
       return false;
     }
 
-    const localStorageIngredients = localStorage.getItem('inProgressRecipes');
-    if (localStorageIngredients) {
-      const inProgressRecipes = JSON.parse(localStorageIngredients);
-      return inProgressRecipes.meals?.[recipeId] || inProgressRecipes.drinks?.[recipeId];
-    }
+  //   const localStorageIngredients = localStorage.getItem('inProgressRecipes');
+  //   if (localStorageIngredients) {
+  //     const inProgressRecipes = JSON.parse(localStorageIngredients);
+  //     return inProgressRecipes.meals?.[recipeId] || inProgressRecipes.drinks?.[recipeId];
+  //   }
 
-    return false;
+  //   return false;
   };
 
   const handleStartRecipeClick = () => {
