1b942861e18346c5552d3fafc943e068699a4b18
MateusDDev <ms4811978@gmail.com>
2023-10-27 08:51:58 -0300

    requisito 32

---
+++ src/components/ShareButton.tsx
@@ -0,0 +1,10 @@
+function ShareButton() {
+  return (
+    <div>
+      <button data-testid="favorite-btn">Favoritar</button>
+      <button data-testid="share-btn">Compartilhar</button>
+    </div>
+  );
+}
+
+export default ShareButton;
