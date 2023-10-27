5d108d8c36397e31b97cb9b11347b2ddaf7ec08a
bernardofortini <bernardofortini04@gmail.com>
2023-10-27 09:59:58 -0300

    Requisito 33

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+
 function ShareButton() {
+  const [message, setMessage] = useState('');
+
+  const shareIcon = '/src/images/shareIcon.svg';
+  const recipeLink = window.location.href;
+
+  const handleShareClick = () => {
+    navigator.clipboard.writeText(recipeLink);
+    setMessage('Link copied!');
+    return message;
+  };
+
   return (
     <div>
       <button data-testid="favorite-btn">Favoritar</button>
-      <button data-testid="share-btn">Compartilhar</button>
+      <button
+        data-testid="share-btn"
+        onClick={ handleShareClick }
+      >
+        <img
+          src={ shareIcon }
+          alt="share button"
+        />
+      </button>
+      <p>{ message && <p>{ message }</p> }</p>
     </div>
   );
 }
