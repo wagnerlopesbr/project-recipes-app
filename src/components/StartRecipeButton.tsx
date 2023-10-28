3e2d703a8ef0e4a4a8b023a74cdb8b39cab372a0
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-10-28 09:36:06 -0300

    Requisito 31 feito

--- src/components/StartRecipeButton.tsx
+++ src/components/StartRecipeButton.tsx
@@ -1,10 +1,12 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import useLocalStorage from '../hooks/useLocalStorage';
 import { DoneRecipesLSType, InProgressRecipesType } from '../Type/type';
+import { s } from 'vitest/dist/types-e3c9754d';
 
 function StartRecipeButton() {
   const [doneRecipes] = useLocalStorage<DoneRecipesLSType[]>('doneRecipes', []);
   const [inProgressRecipes] = useLocalStorage<InProgressRecipesType>('inProgressRecipes');
+  const navigate = useNavigate();
 
   const { pathname } = useLocation();
   const { id } = useParams();
@@ -21,13 +23,28 @@ function StartRecipeButton() {
     return false;
   };
 
+  const handleStartRecipeClick = () => {
+    const key = pathname.includes('meals') ? 'meals' : 'drinks';
+      switch (pathname.includes(key)) {
+        case true: // meals
+          navigate(`/meals/${id}/in-progress`);
+          break;
+        case false: // drinks
+          navigate(`/drinks/${id}/in-progress`);
+          break;
+        default:
+          console.log('GABIGOL, VOLTA A JOGAR BEM PF')
+          break;
+    }
+  };
+
   return (
     <div>
       {!isDone && (
         <button
           style={ { position: 'fixed', bottom: '0px' } }
           data-testid="start-recipe-btn"
-        // onClick={ handleStartRecipeClick }
+          onClick={ handleStartRecipeClick }
         >
           {isRecipeInProgress() ? 'Continue Recipe' : 'Start Recipe'}
         </button>
